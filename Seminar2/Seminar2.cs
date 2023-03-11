6d49950d36dd9d4f2984d93011ca172776be710b
Denis_Gorbachev <mr.pluotttt@gmail.com>
2023-03-11 15:06:58 +0300

    finished

--- Seminar2/Seminar2.cs
+++ Seminar2/Seminar2.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Clear();
+// int n = new Random().Next(100,1000);
+// Console.WriteLine($"Случаной сгенированное число {n}");
+// int n1 = n/100;
+// int n2 = n % 10;
+// Console.WriteLine($"{n1}{n2}");
+
+// Console.Clear();
+// Console.WriteLine("Введите первое число: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// if(n1%n2==0)
+// Console.WriteLine("Второе число кратно первому");
+// else
+// Console.WriteLine("Второе число не кратно первому");
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n % 161 == 0)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n % 7 == 0 && n % 23 == 0)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n * n == m || m * m == n)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+// && - и(одновременное выполнение условий)
+// || - или(выполнение хотя бы одного из условий)
